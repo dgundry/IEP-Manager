39375dea2d20454d286d6744ff254f6682630d02
dgundry <dgundry1@radford.edu>
2022-03-15 09:37:47 -0400

    Fixed Bugs with View Student

--- src/main/java/com/mango/prjmango/ui/students/view/ViewStudentController.java
+++ src/main/java/com/mango/prjmango/ui/students/view/ViewStudentController.java
@@ -14,15 +14,22 @@ public class ViewStudentController {
 
     }
     private void populateTable(ViewStudentView view){
-        String query = "SELECT student_id, first_name, last_name, grade, bio FROM student WHERE teacher_id = " + LoggedInUser.getTeacherId() + ";";
+        String query = "SELECT student_id, first_name, last_name, grade, bio FROM student WHERE teacher_id = ?;";
         try(PreparedStatement statement = DatabaseConnection.getConnection().prepareStatement(query)){
+            statement.setInt(1, LoggedInUser.getTeacherId());
             ResultSet set = statement.executeQuery();
             do{
-                Student s = new Student(set.getInt(1), set.getString(2),
-                        set.getString(3), set.getString(4), set.getString(5));
-                view.getModel().addRow(new Object[] {s.getStudentID(), s.getFirstName(), s.getLastName(), s.getGrade(), s.getBio()});
-            }while(set.next());
-            view.getModel().removeRow(0);
+                try {
+                    Student s = new Student(set.getInt(1), set.getString(2),
+                            set.getString(3), set.getString(4), set.getString(5));
+                    view.getModel().addRow(new Object[]{s.getStudentID(), s.getFirstName(), s.getLastName(), s.getGrade(), s.getBio()});
+                } catch (Exception e){
+                    System.out.println("No Students");
+                }
+                }while(set.next());
+            if(view.getModel().getRowCount() > 1){
+                view.getModel().removeRow(0);
+            }
         }catch(Exception e){
             e.printStackTrace();
         }
