ed3b1f30622e96c6f6bf758c64da2f670aebf217
dgundry <dgundry1@radford.edu>
2022-03-15 12:54:27 -0400

    View Student Data Refactor
    JTable is filled with pre-existing data that is stored, so an additional query is not needed.

--- src/main/java/com/mango/prjmango/ui/students/view/ViewStudentController.java
+++ src/main/java/com/mango/prjmango/ui/students/view/ViewStudentController.java
@@ -1,7 +1,9 @@
 package com.mango.prjmango.ui.students.view;
 
 import com.mango.prjmango.LoggedInUser;
+import com.mango.prjmango.Main;
 import com.mango.prjmango.student.Student;
+import com.mango.prjmango.student.Students;
 import com.mango.prjmango.utilities.DatabaseConnection;
 
 import java.sql.PreparedStatement;
@@ -14,24 +16,28 @@ public class ViewStudentController {
 
     }
     private void populateTable(ViewStudentView view){
-        String query = "SELECT student_id, first_name, last_name, grade, bio FROM student WHERE teacher_id = ?;";
-        try(PreparedStatement statement = DatabaseConnection.getConnection().prepareStatement(query)){
-            statement.setInt(1, LoggedInUser.getTeacherId());
-            ResultSet set = statement.executeQuery();
-            do{
-                try {
-                    Student s = new Student(set.getInt(1), set.getString(2),
-                            set.getString(3), set.getString(4), set.getString(5));
-                    view.getModel().addRow(new Object[]{s.getStudentID(), s.getFirstName(), s.getLastName(), s.getGrade(), s.getBio()});
-                } catch (Exception e){
-                    System.out.println("No Students");
-                }
-                }while(set.next());
-            if(view.getModel().getRowCount() > 1){
-                view.getModel().removeRow(0);
-            }
-        }catch(Exception e){
-            e.printStackTrace();
+//        String query = "SELECT student_id, first_name, last_name, grade, bio FROM student WHERE teacher_id = ?;";
+//        try(PreparedStatement statement = DatabaseConnection.getConnection().prepareStatement(query)){
+//            statement.setInt(1, LoggedInUser.getTeacherId());
+//            ResultSet set = statement.executeQuery();
+//            do{
+//                try {
+//                    Student s = new Student(set.getInt(1), set.getString(2),
+//                            set.getString(3), set.getString(4), set.getString(5));
+//                    view.getModel().addRow(new Object[]{s.getStudentID(), s.getFirstName(), s.getLastName(), s.getGrade(), s.getBio()});
+//                } catch (Exception e){
+//                    System.out.println("No Students");
+//                }
+//                }while(set.next());
+//            if(view.getModel().getRowCount() > 1){
+//                view.getModel().removeRow(0);
+//            }
+//        }catch(Exception e){
+//            e.printStackTrace();
+//        }
+        Students temp = Main.students;
+        for(int i = 0; i < temp.getStudents().size(); i++){
+                    view.getModel().addRow(new Object[]{temp.getStudents().get(i).getStudentID(), temp.getStudents().get(i).getFirstName(), temp.getStudents().get(i).getLastName(), temp.getStudents().get(i).getGrade(), temp.getStudents().get(i).getBio()});
         }
 
     }
